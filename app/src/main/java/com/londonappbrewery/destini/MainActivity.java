69df611be1554aa11fa904de66a6a2aba4999a2d
reliy0n <151805027@stu.adu.edu.tr>
2017-07-16 11:54:09 +0300

    Done!

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,29 +2,126 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
+    // TODO: MY CODE
+    Button btnFirst;
+    Button btnSecond;
+    TextView txtviewQuestion;
+
+    int questionNum;
+    questions question[] = new questions[]{
+            new questions(R.string.T1_Story,R.string.T1_Ans1,R.string.T1_Ans2),
+            new questions(R.string.T2_Story,R.string.T2_Ans1,R.string.T2_Ans2),
+            new questions(R.string.T3_Story,R.string.T3_Ans1,R.string.T3_Ans2),
+            new questions(R.string.T4_End,0,0),
+            new questions(R.string.T5_End,0,0),
+            new questions(R.string.T6_End,0,0),
+
+    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            questionNum = 0;
+        }
+        else{
+            questionNum  = savedInstanceState.getInt("questonKey");
+        }
 
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        btnFirst = (Button) findViewById(R.id.buttonTop);
+        btnSecond = (Button) findViewById(R.id.buttonBottom);
+        txtviewQuestion = (TextView) findViewById(R.id.storyTextView);
 
+        txtviewQuestion.setText(question[questionNum].question);
+        if(questionNum < 3) {
+            btnFirst.setText(question[questionNum].firstAnsw);
+            btnSecond.setText(question[questionNum].secondAnsw);
+        }
+        btnFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getQuestion(1);
+            }
+        });
 
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        btnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getQuestion(2);
+            }
+        });
 
 
     }
+
+    private void getQuestion(int answer){
+        System.out.println("SpeakBefore:"+questionNum);
+        questionNum = findQNumber(questionNum,answer);
+        System.out.println("Speak:"+questionNum);
+        txtviewQuestion.setText(question[questionNum].question);
+        if(questionNum < 3) {
+            btnFirst.setText(question[questionNum].firstAnsw);
+            btnSecond.setText(question[questionNum].secondAnsw);
+        }
+
+    }
+
+    public int findQNumber(int qNumber, int answer){
+        if(qNumber == 0){
+            if(answer == 1){
+                //3
+                return 2;
+            }
+            else if(answer == 2){
+                //2
+                return 1;
+            }
+        }
+        else if(qNumber == 1){
+
+            if(answer == 1){
+                //3
+                return 2;
+            }
+            else if(answer == 2){
+                //4
+                return 3;
+            }
+
+        }
+        else if(qNumber == 2){
+
+            if(answer == 1){
+                //6
+                return 5;
+            }
+            else if(answer == 2){
+                //5
+                return 4;
+            }
+
+        }
+        else{
+            return 0;
+        }
+        return 0;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("questonKey",questionNum);
+    }
+
+
 }
+
---
+++ app/src/main/java/com/londonappbrewery/destini/questions.java
@@ -0,0 +1,21 @@
+package com.londonappbrewery.destini;
+
+import android.widget.Button;
+
+/**
+ * Created by pc on 16.07.2017.
+ */
+
+public class questions {
+    int question;
+    int firstAnsw;
+    int secondAnsw;
+
+    public questions(int question,int firstAnsw,int secondAnsw){
+        this.question = question;
+        this.firstAnsw = firstAnsw;
+        this.secondAnsw = secondAnsw;
+    }
+
+
+}
